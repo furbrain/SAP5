742c4ac6be38be31531098e6dd5d23e3f88b4c4e
Phil Underwood <beardydoc@gmail.com>
2018-08-01 10:56:23 +0100

    Add line drawing function

--- firmware/display.c
+++ firmware/display.c
@@ -369,4 +369,17 @@ void display_clearbuffer_xy(uint8_t x, uint8_t y) {
 	buffer[page][x] &= ~(1 << (y%8));	
 }
 
+void display_draw_line(int x0, int y0, int x1, int y1) {
+    int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
+    int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
+    int err = (dx>dy ? dx : -dy)/2, e2;
+
+    for(;;){
+        display_setbuffer_xy(x0,y0);
+        if (x0==x1 && y0==y1) break;
+        e2 = err;
+        if (e2 >-dx) { err -= dy; x0 += sx; }
+        if (e2 < dy) { err += dx; y0 += sy; }
+    }    
+}
 #endif
