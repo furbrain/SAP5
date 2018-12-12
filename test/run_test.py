baf529dfb1ec7b95ce2f90772c774efc1c66810d
Phil Underwood <beardydoc@gmail.com>
2018-12-12 14:41:08 +0000

    run_test reliably starts mdb_server if not already running

--- test/run_test.py
+++ test/run_test.py
@@ -5,12 +5,18 @@ import socket
 import subprocess
 import time
 import sys
+import os
 s = xmlrpclib.ServerProxy('http://localhost:8888')
 
 try:    
     print s.run(sys.argv[1])
 except socket.error:
-    subprocess.Popen(['python','mdb_server.py'])
+    pid = os.fork()
+    if pid > 0:
+        subprocess.Popen(['python','mdb_server.py'], 
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+        sys.exit()
     time.sleep(2)
     print s.run(sys.argv[1])
 
