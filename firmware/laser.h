17f476cbc60f7abf0911dbde3375297052c15212
Phil Underwood <beardydoc@gmail.com>
2018-12-30 17:42:10 +0000

    Update function comments

--- firmware/laser.h
+++ firmware/laser.h
@@ -8,9 +8,17 @@ enum LASER_SPEED {
     LASER_FAST
     };
 
+/* Turns the laser on or off*/
 void laser_on(bool enable);
+
+/* take a reading with the laser and return the (calibrated) distance
+   throws ERROR_LASER_READ_FAILED if an invalid reading occurred, or took
+   longer than timeout milliseconds*/
 double laser_read(enum LASER_SPEED, int timeout);
+
+#ifdef TEST
 void laser_start(enum LASER_SPEED);
 bool laser_result_ready(void);
 double laser_get_result(void);
+#endif
 #endif
