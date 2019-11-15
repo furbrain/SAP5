efb5b2f1d70f0e68f1e9fe9ab16ee9dca818ed84
Phil Underwood <beardydoc@gmail.com>
2019-11-15 21:12:52 +0000

    Board fixes

---
+++ hardware/merge_files.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+
+def partition(f):
+    f = [x.strip() for x in f]
+    parts = {}
+    parts['header'] = [x for x in f if re.match("M48|METRIC|INCH",x)]
+    parts['def']    = [x for x in f if re.match("T\d+C",x)]
+    parts['drill']  = [x for x in f if re.match("T\d+$|X\d+Y\d+$",x)]
+    return parts
+    
+a = open(sys.argv[1],'r').readlines()
+b = open(sys.argv[2],'r').readlines()
+
+
+a = partition(a)
+b = partition(b)
+if a['header'] != b['header']:
+    raise ValueError("Headers do not match")
+result = a['header'] + a['def'] + b['def'] + ["%"] + a['drill'] + b['drill'] + ["M30"]
+print '\r\n'.join(result)
+
