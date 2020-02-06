be8c61143d636f8e8946adc7c1f6ceecbf60df3e
Phil Underwood <beardydoc@gmail.com>
2020-02-06 10:12:43 +0000

    Remove software folder

--- software/make_exe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wine ~/.wine/drive_c/Python38/Scripts/pyinstaller.exe \
-  --add-binary="libusb-1.0.dll;." \
-  --add-data="manual.pdf;." \
-  --noconfirm \
-  --onefile \
-  PonyTrainer.py
