3d61d406fa4a6f962f9ab9ed4fc6197e013cfc8f
Phil Underwood <beardydoc@gmail.com>
2020-09-18 00:02:49 +0100

    Make data screens more responsive

--- firmware/debug.c
+++ firmware/debug.c
@@ -95,16 +95,22 @@
 //    config.length_units=IMPERIAL;
 //}
 
+
+static int cycle = 1;
+
 void show_raw_sensors(int32_t a) {
     struct COOKED_SENSORS sensors;
     char text[20];
     int i;
-    sensors_read_uncalibrated(&sensors, SAMPLES_PER_READING);
-    display_write_text(0, 0, "Raw:  Mag   Grav", &small_font, false);
-    for(i=0; i<3; i++) {
-        sprintf(text,"%c: %6.2f %6.2f", 'X'+i, sensors.mag[i], sensors.accel[i]);
-        display_write_text(2+2*i, 0, text, &small_font, false);
-    }        
+    if (cycle-- <= 0) {
+        cycle = 10;
+        sensors_read_uncalibrated(&sensors, 3);
+        display_write_text(0, 0, "Raw:  Mag   Grav", &small_font, false);
+        for(i=0; i<3; i++) {
+            sprintf(text,"%c: %6.2f %6.2f", 'X'+i, sensors.mag[i], sensors.accel[i]);
+            display_write_text(2+2*i, 0, text, &small_font, false);
+        }        
+    }
 }
 
 void show_calibrated_sensors(int32_t a) {
@@ -114,95 +120,107 @@ void show_calibrated_sensors(int32_t a) {
     int i;
     char text[20];
     double M, g;
-    sensors_read_cooked(&sensors, SAMPLES_PER_READING);
-    M = gsl_blas_dnrm2(&mag.vector);
-    M /= config.calib.mag[0];
-    g = gsl_blas_dnrm2(&grav.vector);
-    g /= config.calib.accel[0];
-    sprintf(text, "M/g:%6.2f %6.2f", M, g);
-    display_write_text(0,0, text, &small_font, false);
-    for(i=0; i<3; i++) {
-        sprintf(text,"%c:  %6.2f %6.2f", 'X'+i, sensors.mag[i], sensors.accel[i]);
-        display_write_text(2+2*i, 0, text, &small_font, false);
-    }        
-    delay_ms_safe(100);
+    if (cycle-- <= 0) {
+        cycle = 10;
+        sensors_read_cooked(&sensors, 3);
+        M = gsl_blas_dnrm2(&mag.vector);
+        M /= config.calib.mag[0];
+        g = gsl_blas_dnrm2(&grav.vector);
+        g /= config.calib.accel[0];
+        sprintf(text, "M/g:%6.2f %6.2f", M, g);
+        display_write_text(0,0, text, &small_font, false);
+        for(i=0; i<3; i++) {
+            sprintf(text,"%c:  %6.2f %6.2f", 'X'+i, sensors.mag[i], sensors.accel[i]);
+            display_write_text(2+2*i, 0, text, &small_font, false);
+        }        
+    }
 }
 
 void show_details(int32_t a) {
     struct COOKED_SENSORS sensors;
     char text[20];
     double voltage;
-    sensors_read_uncalibrated(&sensors, 3);
-    voltage = battery_get_voltage();
-    struct tm dt;
-    RTCC_TimeGet(&dt);
-    strftime(text, 20, "Time: %H:%M:%S", &dt);
-    display_write_text(0, 0, text, &small_font, false);
-    strftime(text, 20, "Date: %Y-%m-%d", &dt);
-    display_write_text(2, 0, text, &small_font, false);
-    sprintf(text,"Battery: %4.2fv", voltage);
-    display_write_text(4, 0, text, &small_font, false);
-    sprintf(text,"Temp: %4.1f`", sensors.temp);
-    display_write_text(6, 0, text, &small_font, false);
-    delay_ms_safe(500);
+    if (cycle-- <= 0) {
+        cycle = 10;
+        sensors_read_uncalibrated(&sensors, 3);
+        voltage = battery_get_voltage();
+        struct tm dt;
+        RTCC_TimeGet(&dt);
+        strftime(text, 20, "Time: %H:%M:%S", &dt);
+        display_write_text(0, 0, text, &small_font, false);
+        strftime(text, 20, "Date: %Y-%m-%d", &dt);
+        display_write_text(2, 0, text, &small_font, false);
+        sprintf(text,"Battery: %4.2fv", voltage);
+        display_write_text(4, 0, text, &small_font, false);
+        sprintf(text,"Temp: %4.1f`", sensors.temp);
+        display_write_text(6, 0, text, &small_font, false);
+    }
 }
 
 void show_bearings(int32_t a) {
     char stext[80];
     GSL_VECTOR_DECLARE(orientation, 3);
     double compass, inclination;
-    sensors_get_orientation(&orientation, SAMPLES_PER_READING);
-    measure_calculate_bearings(&orientation, &compass, &inclination);
-    sprintf(stext,"Compass: %5.1f`\n  Clino: %+.1f`", compass, inclination);
-    display_write_multiline(2, stext, false);
+    if (cycle-- <= 0) {
+        cycle = 10;
+        sensors_get_orientation(&orientation, 3);
+        measure_calculate_bearings(&orientation, &compass, &inclination);
+        sprintf(stext,"Compass: %5.1f`\n  Clino: %+.1f`", compass, inclination);
+        display_write_multiline(2, stext, false);
+    }
 }
 
 void show_version(int32_t a) {
     char text[40];
-    display_write_text(0, 0, "Hardware", &small_font, false);
-    switch (version_hardware) {
-        case VERSION_ALPHA:
-            strcpy(text, "Alpha");
-            break;
-        case VERSION_V1_0:
-            strcpy(text, "v1.0");
-            break;
-        case VERSION_V1_1:
-            strcpy(text, "v1.1");
-            break;
-        default:
-            strcpy(text, "Unknown");
-            break;
+    if (cycle-- <= 0) {
+        cycle = 10;
+        display_write_text(0, 0, "Hardware", &small_font, false);
+        switch (version_hardware) {
+            case VERSION_ALPHA:
+                strcpy(text, "Alpha");
+                break;
+            case VERSION_V1_0:
+                strcpy(text, "v1.0");
+                break;
+            case VERSION_V1_1:
+                strcpy(text, "v1.1");
+                break;
+            default:
+                strcpy(text, "Unknown");
+                break;
+        }
+        display_write_text(0, 128, text, &small_font, true);
+        sprintf(text, "%hhu.%hhu.%hhu",
+                version_software.version.major,
+                version_software.version.minor,
+                version_software.version.revision);
+        display_write_text(2,0,"Firmware", &small_font, false);
+        display_write_text(2,128,text, &small_font, true);
+        sprintf(text, "Data formats:\n %hhu, %hhu, %hhu", 
+                version_software.config.version,
+                version_software.legs.version,
+                version_software.calibration.version);
+        display_write_multiline(4, text, false);
     }
-    display_write_text(0, 128, text, &small_font, true);
-    sprintf(text, "%hhu.%hhu.%hhu",
-            version_software.version.major,
-            version_software.version.minor,
-            version_software.version.revision);
-    display_write_text(2,0,"Firmware", &small_font, false);
-    display_write_text(2,128,text, &small_font, true);
-    sprintf(text, "Data formats:\n %hhu, %hhu, %hhu", 
-            version_software.config.version,
-            version_software.legs.version,
-            version_software.calibration.version);
-    display_write_multiline(4, text, false);
 }
 
 void show_magnetism(int32_t a) {
     struct COOKED_SENSORS sensors;
-    gsl_vector_view mag = gsl_vector_view_array(sensors.mag, 3);
-    gsl_vector_view grav = gsl_vector_view_array(sensors.accel, 3);
-    char text[40];
-    double M, g, dot, theta;
-    sensors_read_cooked(&sensors, SAMPLES_PER_READING);
-    M = gsl_blas_dnrm2(&mag.vector);
-    g = gsl_blas_dnrm2(&grav.vector);
-    gsl_blas_ddot(&mag.vector, &grav.vector, &dot);
-    theta = acos(dot / M * g) * 360.0 / M_PI;
-    M /= config.calib.mag[0];
-    sprintf(text, "Field: %6.2f<T\nDip:   %5.1f`", M, theta);
-    display_write_multiline(2, text, false);
-    delay_ms_safe(100);
+    if (cycle-- <= 0) {
+        cycle = 10;
+        gsl_vector_view mag = gsl_vector_view_array(sensors.mag, 3);
+        gsl_vector_view grav = gsl_vector_view_array(sensors.accel, 3);
+        char text[40];
+        double M, g, dot, theta;
+        sensors_read_cooked(&sensors, 3);
+        M = gsl_blas_dnrm2(&mag.vector);
+        g = gsl_blas_dnrm2(&grav.vector);
+        gsl_blas_ddot(&mag.vector, &grav.vector, &dot);
+        theta = acos(dot / M * g) * 360.0 / M_PI;
+        M /= config.calib.mag[0];
+        sprintf(text, "Field: %6.2f<T\nDip:   %5.1f`", M, theta);
+        display_write_multiline(2, text, false);
+    }
 }
 
 
@@ -281,7 +299,7 @@ DECLARE_MENU(debug_menu, {
     {"Magnetism", Back, {show_magnetism}, 0},
     {"Misc", Back, {show_details}, 0},
     {"Version", Back, {show_version}, 0},
-    {"Black", Back, {NULL}, 0}
+    {"Back", Back, {NULL}, 0}
 //    {"Battery", Action, {test_battery}, 0},
 //    {"Throw", Action, {throw_error}, 0},
 //    {"Freeze", Action, {freeze_error}, 0},
