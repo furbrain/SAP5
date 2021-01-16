d269cfd6e5dce2d27cdba222e299545c03ec92fb
Phil Underwood <beardydoc@gmail.com>
2021-01-16 00:55:46 +0000

    various updates

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'L6fCl6Ec1XG4+9j0u7QA6ZxcmUZxK+kassybyb0LNGw'
+    APP_NAME = 'PonyTrainer'
+    COMPANY_NAME = 'Underwood Underground'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['http://shetlandattackpony.co.uk/releases/']
---
+++ upload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd "${BASH_SOURCE%/*}/" || exit
+export PYU_SSH_USERNAME=furbrain
+export PYU_SSH_KEYFILE=~/.ssh/id_rsa
+export PYU_SSH_HOST=lynx.mythic-beasts.com
+export PYU_SSH_REMOTE_PATH=/home/furbrain/www/www.shetlandattackpony.co.uk/releases/
+
+mkdir src
+for f in .pyupdater/config.pyu get_versions.py src/__init__.py src/client_config.py
+do
+  rm $f
+  wget https://raw.githubusercontent.com/furbrain/PonyTrainer/master/$f -O $f
+done
+
+python3 get_versions.py
+pyupdater keys -i keypack.pyu
+
+for dir in new deploy files 
+do
+  rm pyu-data/${dir}/*
+done
+
+cp doc/manual.pdf firmware.hex pyu-data/new/
+pyupdater archive --name manual.pdf --version $1
+pyupdater archive --name firmware.hex --version $1
+
+for f in pyu-data/new/*gz
+do
+  cp $f `echo $f | sed -e "s/nix64/win/"`
+done
+
+pyupdater pkg -p -s
+
+pyupdater upload --service scp
